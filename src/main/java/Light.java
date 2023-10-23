f9632c11796ef8ef658e68170d87f4b59047deeb
grasso19 <ant.grasso19@gmail.com>
2023-10-23 16:35:05 +0200

    resolve conflicts for the merge request

--- src/main/java/Light.java
+++ src/main/java/Light.java
@@ -1,4 +1,48 @@
 package main.java;
 
+import java.io.*;
+
 public class Light {
+    private Color color;
+
+    public Light(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void loadPropertiesFromFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                if (words.length >= 1) {
+                    if (words[0].equals("color")) {
+                        if (words.length >= 4) {
+                            try {
+                                double r = Double.parseDouble(words[1]);
+                                double g = Double.parseDouble(words[2]);
+                                double b = Double.parseDouble(words[3]);
+                                color = new Color(r, g, b);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Format de nombre invalide dans la ligne 'color'.");
+                            }
+                        } else {
+                            System.out.println("Format invalide dans la ligne 'color'.");
+                        }
+                    }
+                }
+            }
+
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
