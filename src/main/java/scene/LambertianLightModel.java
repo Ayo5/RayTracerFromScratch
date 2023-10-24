014a457fc755f43037609f3c0bd82bc589b0e778
Vanbandon <theo.vanbandon@gmail.com>
2023-10-24 17:18:15 +0200

    Create Lambert model for light

--- src/main/java/scene/LambertianLightModel.java
+++ src/main/java/scene/LambertianLightModel.java
@@ -1,2 +1,23 @@
-package main.java.scene;public class LambertianLightModel {
+package main.java.scene;
+
+import main.java.coordinate.Vector;
+
+import java.util.List;
+
+public class LambertianLightModel implements LightModel {
+    @Override
+    public Color calculateColor(Vector normal, Color baseColor, List<Light> lights) {
+        Color result = new Color(0,0,0);
+        for (Light light : lights) {
+            Vector lightDirection = light.getDirectionToSurfacePoint();
+            double lightIntensity = light.getIntensity();
+
+            double dotProduct = normal.dot(lightDirection);
+            if (dotProduct > 0) {
+                double LambertFactor = dotProduct;
+                result = result.add(baseColor.multiply(light.getColor()).multiply(LambertFactor * lightIntensity));
+            }
+        }
+        return result;
+    }
 }
