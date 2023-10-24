1ce2f8f533b7f83eff5e5669af264ed8161b28dd
Vanbandon <theo.vanbandon@gmail.com>
2023-10-24 17:12:57 +0200

    Add attribute diffuseColor

--- src/main/java/scene/Material.java
+++ src/main/java/scene/Material.java
@@ -6,10 +6,12 @@ public class Material {
     private main.java.scene.Color color;
     private double reflectivity;
     private double shininess;
+    private Color diffuseColor;
 
-    public Material(Color color) {
+    public Material(Color color, Color diffuseColor) {
         this.color = color;
         this.reflectivity = 0;
         this.shininess = 0;
+        this.diffuseColor = diffuseColor;
     }
 }
