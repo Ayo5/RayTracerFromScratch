9f715d0e64958612f1fa8f9fc83e65924b6db440
Vanbandon <theo.vanbandon@gmail.com>
2023-10-24 17:17:39 +0200

    Create basic model for light

--- src/main/java/scene/BasicLightModel.java
+++ src/main/java/scene/BasicLightModel.java
@@ -1,2 +1,22 @@
-package main.java.scene;public class BasicLightModel {
+package main.java.scene;
+
+import main.java.coordinate.Vector;
+
+import java.util.List;
+
+public class BasicLightModel implements LightModel {
+    @Override
+    public Color calculateColor(Vector normal, Color baseColor, List<Light> lights) {
+        Color result = new Color(0,0,0);
+        for (Light light : lights) {
+            Vector lightDirection = light.getDirection();
+            double lightIntensity = light.getIntensity();
+
+            double dotProduct = normal.dotScalar(lightDirection);
+            if (dotProduct > 0) {
+                result = result.add(baseColor.multiply(light.getColor()).multiply(dotProduct * lightIntensity));
+            }
+        }
+        return result;
+    }
 }
