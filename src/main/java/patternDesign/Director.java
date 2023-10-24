eb519cdfd4181ade7289f9dc9be0186a11af2178
Vanbandon <theo.vanbandon@gmail.com>
2023-10-24 10:17:12 +0200

    Director class for pattern

---
+++ src/main/java/patternDesign/Director.java
@@ -0,0 +1,4 @@
+package main.java.patternDesign;
+
+public class Director {
+}
---
+++ src/main/java/patternDesign/Product.java
@@ -0,0 +1,10 @@
+package main.java.patternDesign;
+
+import main.java.Triangle;
+
+public class Client {
+    director = new Director();
+    TriangleBuilder triangle = new TriangleBuilder();
+    director.makeTriangle(builder);
+    Triangle triangle = builder.getResult();
+}
