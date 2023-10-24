9613d1247a3cf8153a92644b80c13df6892f98a9
Ayoub Daï <daiayoub59@gmail.com>
2023-10-24 08:59:30 +0200

    Creation of the class Plane with test

---
+++ src/main/java/Plane.java
@@ -0,0 +1,57 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Plan {
+    private Point a ;
+    private Point b ;
+
+
+    public Point getA() {
+        return a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public Plan() {
+        this.a = null ;
+        this.b = null ;
+    }
+
+    public void settingFromFile(String fileName) {
+        try {
+            File file = new File("src/main/ressource/" + fileName);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                if (words.length > 0) {
+                    String keyword = words[0];
+                    if (keyword.equals("plane")) {
+                        try {
+                            double x = Double.parseDouble(words[1]);
+                            double y = Double.parseDouble(words[2]);
+                            double z = Double.parseDouble(words[3]);
+                            double u = Double.parseDouble(words[4]);
+                            double v = Double.parseDouble(words[5]);
+                            double w = Double.parseDouble(words[6]);
+                            a = new Point(x, y, z);
+                            b = new Point(u, v, w);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Format de nombre invalide dans la ligne de la sphère.");
+                        }
+                    }
+
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Erreur de lecture du fichier : " + e.getMessage());
+        }
+    }
+}
