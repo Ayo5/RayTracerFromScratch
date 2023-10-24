72350c77fbea0955e9599de76a53c7142472aea3
Vanbandon <theo.vanbandon@gmail.com>
2023-10-24 17:16:37 +0200

    Add attribute direction

--- src/main/java/scene/DirectionalLight.java
+++ src/main/java/scene/DirectionalLight.java
@@ -4,13 +4,17 @@ import main.java.coordinate.Vector;
 
 public class DirectionalLight extends Light {
     private Vector direction;
+    private Color color;
 
     public DirectionalLight(Color color, Vector direction) {
         super(color);
         this.direction = direction;
+        this.color = color;
     }
 
     public Vector getDirection() {
         return direction;
     }
+
+    public Color getColor() { return color; }
 }
